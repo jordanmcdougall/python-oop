e9324cbd0411211dc9c37d1c936f29f5312effa6
Jordan McDougall <jordanmcdougall@hotmail.co.uk>
2019-01-11 00:35:14 +0000

    Adding Classes and Instances Lesson

---
+++ 1_Classes_And_Instances.py
@@ -0,0 +1,37 @@
+#https://www.youtube.com/watch?v=ZDa-Z5JzLYM
+
+class ClassEmpty:
+    
+    # You can use pass to stop python throwing errors for empty classes
+    pass
+
+class Employee:
+    
+    def __init__(self, firstname,lastname, age):
+        #Define attributes for class
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.email = firstname + '.' + lastname + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.firstname, self.lastname)
+
+#Don't need to pass the instance "self" as an arg as it's done automatically
+emp_1 = Employee('Jordan', 'McDougall', 25)
+
+emp_2= Employee('Employee', 'Two', 26)
+
+print('Printing Employee Email using class attribute')
+print(emp_1.email)
+print(emp_2.email)
+
+print('\nPrinting Employee full name using class method')
+print(emp_1.fullname())
+print(emp_2.fullname())
+
+### 
+# Notes
+# * Every method in a class automatically takes the instance as the 1st arguement (i.e.self)
+# * When invoking attributes you don't need parenthesis, for methods you do.
+###
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# python-oop
-Object Orientated Programming in Python
+# Object Orientated Programming in Python
+
+## * Tutorials following Cody Schafer's YT playlist
+
+1. [Classes and Instances](1_Classes_And_Instances.py)
